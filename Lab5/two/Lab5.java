0e4bbbedd9a8b6fc0d461930300002f884d89554
unknown <djones20@CSPC046.dcs.bbk.ac.uk>
2015-10-26 20:57:29 +0000

    Finished Lab5

--- Lab5/two/Lab5.java
+++ Lab5/two/Lab5.java
@@ -5,17 +5,17 @@ public class Lab5 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Lab 5.2.2 - Fibonacci");
-        System.out.println("Total: " + fibonacci(10));
+        System.out.println("Lab5.2.3 - Hanoi towers");
+        System.out.println("The number of moves for 64 discs is: " + hanoi(1));
         System.out.println("Goodbye.");
+        int i = 64;
     }
 
-    private static int fibonacci (int n) {
-        if (n == 1 || n == 2) {
+    private static int hanoi (int n) {
+        System.out.println(i);
+        if (n == 1) {
             return 1;
         } else {
-            int result = fibonacci(n-1) + fibonacci(n-2);
-            return result;
         }
     }
 }
