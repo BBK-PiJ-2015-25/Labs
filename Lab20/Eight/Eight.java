f51b3ce885c4f6fda79c2fdce3f6f7b649d1abef
David Jones <davidjones889@googlemail.com>
2016-03-07 20:11:10 +0000

    Added a solution to question 8

--- Lab20/Eight/Eight.java
+++ Lab20/Eight/Eight.java
@@ -1,10 +1,22 @@
+import java.util.List;
+import java.util.stream.*;
+import java.util.Arrays;
+import java.util.Optional;
+
 public class Eight {
 
 	public static void main(String[] args) {
 		System.out.println("Starting...");
 		System.out.println("..............");
 
-		
+		List<String> words = Arrays.asList("hi", "four", "hello", "world", "foo", "bar", "hi five", "as", "elephant", "house", "ear", "bee");
+
+		Optional<String> concatWords = words.stream().map(String::toUpperCase)
+													 .reduce((a, b) -> a + b);
+
+		if (concatWords.isPresent()) {
+			System.out.println(concatWords.get());
+		}
 
 		System.out.println("..............");
 		System.out.println("Finished");
